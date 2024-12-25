d403ba2590d3fbb26d66ab898cfb0f018996cac1
DevloperMM <mangalmurtivarshney2810@gmail.com>
2024-12-25 23:47:31 +0530

    Logo import corrected

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import myLogo from "../assets/logo.png";
-=======
-import myLogo from "../assets/img1.jpg";
->>>>>>> 68ba8a40a0391c7ed2aa547eb9d64e7023a21c5b
 
 function Logo({ width = "100px" }) {
   return (
