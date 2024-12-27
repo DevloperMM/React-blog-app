5af81739bf63c11c7cfc2220bdc13dc46558c37c
DevloperMM <mangalmurtivarshney2810@gmail.com>
2024-12-27 10:09:18 +0530

    Fixes edit and del btn issue

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -47,7 +47,7 @@ function Login() {
           </Link>
         </p>
 
-        {error && <p className="text-red-600 mt-8 text-center"></p>}
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
 
         <form onSubmit={handleSubmit(login)} className="mt-8">
           <div className="space-y-5">
@@ -60,7 +60,7 @@ function Login() {
                 validate: {
                   matchPattern: (value) =>
                     /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                    "Email address must be valid address",
+                    "This field must contain valid address",
                 },
               })}
             />
@@ -70,7 +70,7 @@ function Login() {
               placeholder="Enter your password"
               {...register("password", { required: true })}
             />
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="w-full hover:bg-opacity-75">
               Sign in
             </Button>
           </div>
